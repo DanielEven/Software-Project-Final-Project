b715970b24505ffc7d4374304bc2f5b068b135d4
Liam Mohr <liam.mohr@gmail.com>
2022-02-04 22:50:41 +0200

    Changed functions to use pointers

--- matrix.c
+++ matrix.c
@@ -1,61 +1,61 @@
 #include "matrix.h"
 
-Matrix alloc_matrix(int m, int n)
+Matrix *alloc_matrix(int m, int n)
 {
     int i;
-    Matrix to;
+    Matrix *to = malloc(sizeof(Matrix));
 
-    to.m = m;
-    to.n = n;
-    to.vals = calloc(m, sizeof(double *));
-    if (!to.vals)
+    to->m = m;
+    to->n = n;
+    to->vals = calloc(m, sizeof(double *));
+    if (!to->vals)
     {
         /* TODO handle error.*/
     }
     for (i = 0; i < m; i++)
     {
-        to.vals[i] = calloc(n, sizeof(double));
-        if (!to.vals[i])
+        to->vals[i] = calloc(n, sizeof(double));
+        if (!to->vals[i])
         {
             /* TODO handle error.*/
         }
     }
     return to;
 }
-void free_matrix(Matrix a)
+void free_matrix(Matrix *a)
 {
     int i;
 
-    for (i = 0; i < a.m; i++)
-        free(a.vals[i]);
-    free(a.vals);
+    for (i = 0; i < a->m; i++)
+        free(a->vals[i]);
+    free(a->vals);
 }
 
-Matrix dup_matrix(Matrix a)
+Matrix *dup_matrix(Matrix *a)
 {
     int i, j;
-    Matrix to;
+    Matrix *to;
 
-    to = alloc_matrix(a.m, a.n);
-    for (i = 0; i < a.m; i++)
-        for (j = 0; j < a.n; j++)
-            to.vals[i][j] = a.vals[i][j];
+    to = alloc_matrix(a->m, a->n);
+    for (i = 0; i < a->m; i++)
+        for (j = 0; j < a->n; j++)
+            to->vals[i][j] = a->vals[i][j];
     return to;
 }
-Matrix matrix_from_arr(double **arr, int m, int n)
+Matrix *matrix_from_arr(double **arr, int m, int n)
 {
     int i, j;
-    Matrix to;
+    Matrix *to;
 
     to = alloc_matrix(m, n);
     for (i = 0; i < m; i++)
         for (j = 0; j < n; j++)
-            to.vals[i][j] = arr[i][j];
+            to->vals[i][j] = arr[i][j];
     return to;
 }
-Matrix matrix_from_PyList(PyObject *lst)
+Matrix *matrix_from_PyList(PyObject *lst)
 {
-    Matrix to;
+    Matrix *to;
     Py_ssize_t i, j, m, n;
 
     m = PyList_Size(lst);
@@ -66,78 +66,81 @@ Matrix matrix_from_PyList(PyObject *lst)
     {
         for (j = 0; j < n; j++)
         {
-            to.vals[i][j] = PyFloat_AsDouble(PyList_GetItem(PyList_GetItem(lst, i), j));
+            to->vals[i][j] = PyFloat_AsDouble(PyList_GetItem(PyList_GetItem(lst, i), j));
         }
     }
     return to;
 }
 
-void add_matrix(Matrix res, Matrix a, Matrix b)
+void add_matrix(Matrix *res, Matrix *a, Matrix *b)
 {
     int i, j;
 
     assert(SAME_SIZE(a, b));
-    res = alloc_matrix(a.m, a.n);
-    for (i = 0; i < a.m; i++)
-        for (j = 0; j < a.n; j++)
-            res.vals[i][j] = a.vals[i][j] + b.vals[i][j];
+    res = alloc_matrix(a->m, a->n);
+    for (i = 0; i < a->m; i++)
+        for (j = 0; j < a->n; j++)
+            res->vals[i][j] = a->vals[i][j] + b->vals[i][j];
 }
-void add_matrix_inp(Matrix a, Matrix b)
+
+void add_matrix_inp(Matrix *a, Matrix *b)
 {
     int i, j;
 
     assert(SAME_SIZE(a, b));
-    for (i = 0; i < a.m; i++)
-        for (j = 0; j < a.n; j++)
-            a.vals[i][j] += b.vals[i][j];
+    for (i = 0; i < a->m; i++)
+        for (j = 0; j < a->n; j++)
+            a->vals[i][j] += b->vals[i][j];
 }
 
-void sub_matrix(Matrix res, Matrix a, Matrix b)
+void sub_matrix(Matrix *res, Matrix *a, Matrix *b)
 {
     int i, j;
 
     assert(SAME_SIZE(a, b));
-    res = alloc_matrix(a.m, a.n);
-    for (i = 0; i < a.m; i++)
-        for (j = 0; j < a.n; j++)
-            res.vals[i][j] = a.vals[i][j] - b.vals[i][j];
+    res = alloc_matrix(a->m, a->n);
+    for (i = 0; i < a->m; i++)
+        for (j = 0; j < a->n; j++)
+            res->vals[i][j] = a->vals[i][j] - b->vals[i][j];
 }
-void sub_matrix_inp(Matrix a, Matrix b)
+
+void sub_matrix_inp(Matrix *a, Matrix *b)
 {
     int i, j;
 
     assert(SAME_SIZE(a, b));
-    for (i = 0; i < a.m; i++)
-        for (j = 0; j < a.n; j++)
-            a.vals[i][j] -= b.vals[i][j];
+    for (i = 0; i < a->m; i++)
+        for (j = 0; j < a->n; j++)
+            a->vals[i][j] -= b->vals[i][j];
 }
 
-void dot_matrix(Matrix res, Matrix a, Matrix b)
+void dot_matrix(Matrix *res, Matrix *a, Matrix *b)
 {
     int i, j, k;
 
     assert(GOOD_FOR_DOT(a, b));
-    res = alloc_matrix(a.m, b.n);
-    for (i = 0; i < a.m; i++)
-        for (j = 0; j < b.n; j++)
-            for (k = 0; k < a.n; k++)
-                res.vals[i][j] += a.vals[i][k] * b.vals[k][j];
+    res = alloc_matrix(a->m, b->n);
+    for (i = 0; i < a->m; i++)
+        for (j = 0; j < b->n; j++)
+            for (k = 0; k < a->n; k++)
+                res->vals[i][j] += a->vals[i][k] * b->vals[k][j];
 }
 
-void mult_matrix(Matrix res, Matrix a, double scalar)
+void mult_matrix(Matrix *res, Matrix *a, double scalar)
 {
     int i, j;
 
-    res = alloc_matrix(a.m, a.n);
-    for (i = 0; i < a.m; i++)
-        for (j = 0; j < a.n; j++)
-            res.vals[i][j] = a.vals[i][j] * scalar;
+    res = alloc_matrix(a->m, a->n);
+    for (i = 0; i < a->m; i++)
+        for (j = 0; j < a->n; j++)
+            res->vals[i][j] = a->vals[i][j] * scalar;
 }
-void mult_matrix_inp(Matrix a, double scalar)
+
+void mult_matrix_inp(Matrix *a, double scalar)
 {
     int i, j;
 
-    for (i = 0; i < a.m; i++)
-        for (j = 0; j < a.n; j++)
-            a.vals[i][j] *= scalar;
+    for (i = 0; i < a->m; i++)
+        for (j = 0; j < a->n; j++)
+            a->vals[i][j] *= scalar;
 }
--- matrix.h
+++ matrix.h
@@ -15,32 +15,32 @@ typedef struct Matrix
 } Matrix;
 
 /* Matrix memory management. */
-Matrix alloc_matrix(int m, int n);
-void free_matrix(Matrix a);
+Matrix *alloc_matrix(int m, int n);
+void free_matrix(Matrix *a);
 
 /* Creating a matrix.*/
-Matrix dup_matrix(Matrix a);
-Matrix matrix_from_arr(double **arr, int m, int n);
-Matrix matrix_from_PyList(PyObject *lst);
+Matrix *dup_matrix(Matrix *a);
+Matrix *matrix_from_arr(double **arr, int m, int n);
+Matrix *matrix_from_PyList(PyObject *lst);
 
 /* Operations over matrices. 
 Every function has 2 versions:
 1. An in place version - inp.
 2. A vestion with a result parameter.
 */
-void add_matrix(Matrix res, Matrix a, Matrix b);
-void add_matrix_inp(Matrix a, Matrix b);
+void add_matrix(Matrix *res, Matrix *a, Matrix *b);
+void add_matrix_inp(Matrix *a, Matrix *b);
 
-void sub_matrix(Matrix res, Matrix a, Matrix b);
-void sub_matrix_inp(Matrix a, Matrix b);
+void sub_matrix(Matrix *res, Matrix *a, Matrix *b);
+void sub_matrix_inp(Matrix *a, Matrix *b);
 
-void dot_matrix(Matrix res, Matrix a, Matrix b);
+void dot_matrix(Matrix *res, Matrix *a, Matrix *b);
 
-void mult_matrix(Matrix res, Matrix a, double scalar);
-void mult_matrix_inp(Matrix a, double scalar);
+void mult_matrix(Matrix *res, Matrix *a, double scalar);
+void mult_matrix_inp(Matrix *a, double scalar);
 
 /* Size checking macros.*/
-#define SAME_SIZE(a, b) ((((a).m) == ((b).m)) && (((a).n) == ((b).n)))
-#define GOOD_FOR_DOT(a, b) (((a).n) == ((b).m))
+#define SAME_SIZE(a, b) ((((a)->m) == ((b)->m)) && (((a)->n) == ((b)->n)))
+#define GOOD_FOR_DOT(a, b) (((a)->n) == ((b)->m))
 
 #endif
