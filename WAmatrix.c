a4955d6bcd00144cc93833f6246268684a47a6b7
DanielEven <danieleven@mail.tau.ac.il>
2022-02-11 14:47:54 +0200

    Fixed a bug in WA matrix.
    We forgot this part - it's written in the docs.

--- WAmatrix.c
+++ WAmatrix.c
@@ -11,7 +11,8 @@ Matrix *create_WA_matrix(Matrix *vects_rows)
     {
         for (j = 0; j < vects_rows->m; j++)
         {
-            res->vals[i][j] = exp(-get_dist(vects_rows->vals[i], vects_rows->vals[j], vects_rows->n) / 2);
+            if (i != j) /* If i=j, W_ij is defined as 0. */
+                res->vals[i][j] = exp(-get_dist(vects_rows->vals[i], vects_rows->vals[j], vects_rows->n) / 2);
         }
     }
 
