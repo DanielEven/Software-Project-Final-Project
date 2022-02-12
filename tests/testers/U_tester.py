cb70c7260b912e3e6ea9843321d2ddf25e6eeefd
DanielEven <danieleven@mail.tau.ac.il>
2022-02-12 16:47:23 +0200

    Added a tester for eigen.

---
+++ tests/testers/U_tester.py
@@ -0,0 +1,30 @@
+import sys
+import os
+
+bad = False
+
+os.system('./out >> tests/testers/U_res.txt')
+
+with open("tests/expected_U.txt", 'r') as out, open("tests/testers/U_res.txt", 'r') as res:
+    nums_out_lines = out.read().split('\n')
+    nums_res_lines = res.read().split('\n')
+
+    for i in range(len(nums_out_lines)):
+        nums_out = nums_out_lines[i].split(' ')
+        nums_res = nums_res_lines[i].split(' ')
+
+        for j in range(len(nums_out)):
+            a = nums_out[j]
+            b = nums_res[j]
+            if a != b:
+                '''
+                val = abs(float(a)-float(b))
+                if val <= 0.00011:
+                    print(f"DIFF: {a}, {b}")
+                else:
+                '''
+                print(f"BAD DIFF: {a}, {b}")
+                bad = True
+
+if bad:
+    sys.exit(1)
