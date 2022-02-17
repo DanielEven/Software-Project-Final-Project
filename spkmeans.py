bb2dc5480080f51426f344b35edd55e204140d09
DanielEven <danieleven@mail.tau.ac.il>
2022-02-17 18:53:01 +0200

    Getting arguments in the python file.
    Co-authored-by: liammohr <liammohr@users.noreply.github.com>

--- spkmeans.py
+++ spkmeans.py
@@ -1,1 +1,22 @@
 # Python interface of the code.
+import sys
+
+
+def get_arguments():
+    argc = len(sys.argv)
+
+    try:
+        if argc != 4:  # max_iter is not provided.
+            raise ValueError()
+
+        k = int(sys.argv[1])
+        goal = sys.argv[2]
+        file_name = sys.argv[3]
+        assert(goal in ["spk", "wam", "ddg", "lnorm", "jacobi"]) # Checking enum
+        assert(file_name.endswith(".txt") or file_name.endswith(".csv"))  # Checking the file type.
+
+        return k, goal, file_name
+    except (ValueError,  AssertionError):
+        print("Invalid Input!")
+        sys.exit(1)
+
