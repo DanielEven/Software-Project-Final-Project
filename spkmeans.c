d6232d08bad9b1acca823f3b4df0dc1f5cb885d3
DanielEven <danieleven@mail.tau.ac.il>
2022-02-24 20:36:35 +0200

    Started implementing calculate_requested.
    Co-authored-by: liammohr <liammohr@users.noreply.github.com>

--- spkmeans.c
+++ spkmeans.c
@@ -2,9 +2,32 @@
 /* Has a function to manage the calls to all of the C files. */
 
 #include "spkmeans.h"
-#include "vector.h"
 
 Matrix *calculate_requested(Matrix *data_matrix, Py_ssize_t k, const char *goal)
 {
+    Matrix *WA, *DD, *NGL;
+
+    WA = create_WA_matrix(data_matrix);
+    if (!strcmp(goal, "wam"))
+    {
+        return WA;
+    }
+
+    DD = create_DD_matrix(WA);
+    if (!strcmp(goal, "ddg"))
+    {
+        free_matrix(WA);
+        return DD;
+    }
+
+    NGL = create_NGL_matrix(WA, DD);
+    free_matrix(WA);
+    free_matrix(DD);
+
+    if (!strcmp(goal, "lnorm"))
+    {
+        return NGL;
+    }
+
     return NULL;
 }
--- spkmeans.h
+++ spkmeans.h
@@ -1,4 +1,11 @@
 #include "matrix.h"
+#include "vector.h"
+#include "WAmatrix.h"
+#include "NGLmatrix.h"
+#include "Umatrix.h"
+#include "Tmatrix.h"
+
+#include <string.h>
 
 /* Afunction to calculate the result, by sending the data_matrix to the right function - according to goal. */
 Matrix *calculate_requested(Matrix *data_matrix, Py_ssize_t k, const char *goal);
