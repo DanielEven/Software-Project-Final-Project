b4b77c70a303d4063de8e5608113b47ef0b68033
DanielEven <danieleven@mail.tau.ac.il>
2022-04-23 13:15:26 +0300

    Added bash title.

--- comp.sh
+++ comp.sh
@@ -1,3 +1,5 @@
+#! /bin/bash
+
 # The compilation script.
 gcc -ansi -Wall -Wextra -Werror -pedantic-errors kmeans.c matrix.c NGLmatrix.c spkmeans.c Tmatrix.c Umatrix.c vector.c WAmatrix.c \
      -lm -o spkmeans
